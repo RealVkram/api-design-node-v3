0e21b9e4ac78c28bfb32363143c4ce88ba7913da
RealVkram <ajayiadegbemi.com>
2020-08-29 10:09:15 +0100

    add all routes and controllers

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,41 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send('email and password required')
+  }
+  try {
+    const user = await User.create(req.body)
+    const token = newToken(user)
+    return res.status(200).send({ data: token })
+  } catch (e) {
+    return res /* allways use return statment */
+      .status(400)
+      .end() /* .send -- sends back a data and .end sends no data back */
+  }
+}
 
 export const signin = async (req, res) => {}
 
 export const protect = async (req, res, next) => {
+  let token = req.headers.authorization.split('Bearer ')[1]
+
+  if (!token) {
+    return res.status(401).end()
+  }
+
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean() /* converts it to json */
+      .exec()
+    req.user = user
+    next()
+  } catch (e) {
+    console.error(e)
+    return res.status(401).end()
+  }
   next()
 }
